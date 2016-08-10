6c2bd014d32cd4ea9b39dfc52127732409f3e9fa
NEETHU09 <neethulakshman7@gmail.com>
2016-08-10 10:27:22 +0530

    excel to json nodejs program

---
+++ gulpFile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+
+gulp.task('browserify', function(){
+    browserify('./client/src/js/app.js')
+        .transform('reactify')
+        .bundle()
+        .pipe(source('api.js'))
+        .pipe(gulp.dest('./client/dist/js'))
+});
+
+gulp.task('copy', function(){
+    gulp.src('./client/src/*.html').pipe(gulp.dest('./client/dist'));
+    gulp.src('./client/src/css/*.*').pipe(gulp.dest('./client/dist/css'));
+    gulp.src('./client/src/fonts/*.*').pipe(gulp.dest('./client/dist/fonts'));
+    gulp.src('./client/src/images/*.*').pipe(gulp.dest('./client/dist/images'));
+    gulp.src('./client/src/js/vendor/*.*').pipe(gulp.dest('./client/dist/js/vendor'));
+});
+
+gulp.task('default',['browserify','copy'], function(){
+    return gulp.watch('src/**/*.*',['browserify','copy']);
+});
