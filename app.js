56e3b7e8406c05cefc69df2f97cf123d32bfa9a4
NEETHU09 <neethulakshman7@gmail.com>
2016-08-12 17:24:32 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-
-var app = express();
-var routes = require('./Server/routes/employees');
-console.log("inside app js");
-app.use('/employee', routes);
-app.listen(9090);
