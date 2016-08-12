81fbdd3e2a315d33c1c3a90d5839bb346a522d9c
NEETHU09 <neethulakshman7@gmail.com>
2016-08-12 17:32:23 +0530

    Add files via upload

---
+++ Client/src/js/pages/BarGraph.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var Navbar=require('../components/Navbar');
+var BarGraph = React.createClass({
+  render () {
+    return (
+      <div>
+        Hi
+      </div>
+    );
+  }
+  });
+
+  module.exports = BarGraph;
